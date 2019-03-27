bd45c7dabd97e9a783d93fcf92ec7ca4f628d41e
Timur Garipov <tbgaripov@gmail.com>
2019-03-27 19:05:24 +0300

    Initial Commit

---
+++ ATD_DICTIONARY/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ATD_DICTIONARY
+//
+//  Created by Тимур Гарипов on 27/03/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
